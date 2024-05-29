cb37c93d87620a5c9f260cfd78dc8888d6924164
StasAnanas666 <lapshin-5a@yandex.ru>
2024-05-29 12:24:44 +0300

    next ex

---
+++ src/app/dashboard/profile/info.jsx
@@ -0,0 +1,8 @@
+export default function Info() {
+    return (<div>
+        <h4>Имя пользователя: </h4>
+        <p>Электронная почта: </p>
+        <p>Телефон: </p>
+    </div>
+    )
+}
---
+++ src/app/dashboard/profile/page.jsx
@@ -0,0 +1,10 @@
+import Info from "./info"
+
+export default function Profile() {
+    return (
+        <div>
+            <h2>Profile</h2>
+            <Info />
+        </div>
+    )
+}
